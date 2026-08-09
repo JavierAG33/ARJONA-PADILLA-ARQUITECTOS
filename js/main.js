0eb81b4cecb00beeec973c7a3a5335cd54f8984c
JavierAG33 <jarjonagil@gmail.com>
2026-08-09 10:48:42 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -89,6 +89,31 @@ document.addEventListener("DOMContentLoaded", () => {
           showSlide(currentSlide + 1);
         }
       });
+      document.addEventListener("DOMContentLoaded", () => {
+  const homeSlider = document.querySelector(".home-slider");
+
+  if (!homeSlider) {
+    return;
+  }
+
+  const slides = homeSlider.querySelectorAll(".home-slide");
+
+  if (slides.length < 2) {
+    return;
+  }
+
+  let currentSlide = 0;
+
+  function showNextSlide() {
+    slides[currentSlide].classList.remove("is-active");
+
+    currentSlide = (currentSlide + 1) % slides.length;
+
+    slides[currentSlide].classList.add("is-active");
+  }
+
+  setInterval(showNextSlide, 4500);
+});
     }
   }
 });
